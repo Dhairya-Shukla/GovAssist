ed890772a64f3c5363b7570787fff17ebad3b8a1
Dhairya Shukla <dhairyashukla1702@gmail.com>
2026-06-12 18:40:01 +0530

    Added initial scheme dataset

---
+++ data/schemes_data.py
@@ -0,0 +1,165 @@
+schemes = [
+
+    {
+        "name": "PM-KISAN",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Kisan Credit Card Scheme",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Pradhan Mantri Fasal Bima Yojana",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Soil Health Card Scheme",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Agriculture Infrastructure Fund",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Paramparagat Krishi Vikas Yojana",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "National Mission for Sustainable Agriculture",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "e-NAM",
+        "category": "Farmer",
+        "occupation": "Farmer"
+    },
+
+    {
+        "name": "Pradhan Mantri Matru Vandana Yojana",
+        "category": "Pregnant Women",
+        "gender": "Female"
+    },
+
+    {
+        "name": "Janani Suraksha Yojana",
+        "category": "Pregnant Women",
+        "gender": "Female"
+    },
+
+    {
+        "name": "Janani Shishu Suraksha Karyakram",
+        "category": "Pregnant Women",
+        "gender": "Female"
+    },
+
+    {
+        "name": "SUMAN Scheme",
+        "category": "Pregnant Women",
+        "gender": "Female"
+    },
+
+    {
+        "name": "National Scholarship Portal",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "PM YASASVI Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Central Sector Scholarship Scheme",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "INSPIRE Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Pragati Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Saksham Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Post Matric Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Merit Cum Means Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "AICTE Swanath Scholarship",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Vidyanjali Scheme",
+        "category": "Student",
+        "occupation": "Student"
+    },
+
+    {
+        "name": "Ayushman Bharat",
+        "category": "Healthcare",
+        "occupation": "Any"
+    },
+
+    {
+        "name": "PM Jan Arogya Yojana",
+        "category": "Healthcare",
+        "occupation": "Any"
+    },
+
+    {
+        "name": "eSanjeevani",
+        "category": "Healthcare",
+        "occupation": "Any"
+    },
+
+    {
+        "name": "National TB Elimination Programme",
+        "category": "Healthcare",
+        "occupation": "Any"
+    },
+
+    {
+        "name": "Rashtriya Bal Swasthya Karyakram",
+        "category": "Healthcare",
+        "occupation": "Any"
+    }
+
+]
